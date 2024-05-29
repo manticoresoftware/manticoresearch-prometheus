040af3f481437d967f6ddfe3590d9ac3551e5249
djklim87 <klim@manticoresearch.com>
2024-05-29 22:36:51 +0200

    Tag build as 6.3.0 release

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-BUILD_TAG=6.2.13.1
+BUILD_TAG=6.3.0.0
 echo $BUILD_TAG
 docker build --platform="linux/amd64" --no-cache -t manticoresearch/prometheus-exporter:$BUILD_TAG .
 docker push manticoresearch/prometheus-exporter:$BUILD_TAG
