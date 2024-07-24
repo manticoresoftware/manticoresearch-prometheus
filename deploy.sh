3441d0d88c4a2b209160bc097e12c9fde9738264
yonny <janzisko@gmail.com>
2024-07-24 13:53:47 +0200

    feat(arm64): multi platform build

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 BUILD_TAG=6.3.2.0
+DOCKER_REPO=manticoresearch/prometheus-exporter
 echo $BUILD_TAG
-docker build --platform="linux/amd64" --no-cache -t manticoresearch/prometheus-exporter:$BUILD_TAG .
-docker push manticoresearch/prometheus-exporter:$BUILD_TAG
+echo $DOCKER_REPO
+
+docker buildx create --use
+docker buildx build \
+    --platform linux/amd64,linux/arm64 \
+    --output type=registry \
+    --push \
+    --progress auto \
+    --no-cache -t $DOCKER_REPO:$BUILD_TAG .
